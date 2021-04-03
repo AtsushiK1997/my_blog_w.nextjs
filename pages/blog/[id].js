a2ba9b7722c090f94a3460cd6ca5dfadf785ccd0
11kkum <kikuchi.atsushi@lev.co.jp>
2021-04-03 19:26:06 +0900

    add 404

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,44 +1,44 @@
-export default function BlogId({blog}){
-    return(
+export default function BlogId({ blog }) {
+    return (
         <main>
             <h1>{blog.title}</h1>
-            <P>{blog.pulishedAt}</P>
+            <p>{blog.publishedAt}</p>
             <div
                 dangerouslySetInnerHTML={{
-                    __html:`${blog.body}`,
+                    __html: `${blog.body}`,
                 }}
             />
         </main>
     );
 }
 
-// 静的生成のためのパスを指定します
-export const getStaticPaths = async() => {
+  // 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
     const key = {
-        headers:{'X-API-KEY':process.env.API_KEY},
+        headers: {'X-API-KEY': process.env.API_KEY},
     };
-    const data = await fetch('https://kikuchi.microcms.io/api/v1/blog',key)
+    const data = await fetch('https://kikuchi.microcms.io/api/v1/blog', key)
         .then(res => res.json())
         .catch(() => null);
-    const paths = data.contents.map(content => `blog/${content.id}`);
-    return {paths,fallback:  false};
+    const paths = data.contents.map(content => `/blog/${content.id}`);
+    return {paths, fallback: false};
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async context => {
-    const id =context.params.id;
+    const id = context.params.id;
     const key = {
-        headers:{'X-API-KEY': process.env.API_KEY},
+        headers: {'X-API-KEY': process.env.API_KEY},
     };
     const data = await fetch(
-        'https://kikuchi.microcms.io/api/v1/blog' + id,
+        'https://kikuchi.microcms.io/api/v1/blog/' + id,
         key,
     )
-    .then(res => res.json())
-    .catch(() => null);
-    return{
-        props:{
-            blog:data,
+        .then(res => res.json())
+        .catch(() => null);
+    return {
+        props: {
+            blog: data,
         },
     };
 };
